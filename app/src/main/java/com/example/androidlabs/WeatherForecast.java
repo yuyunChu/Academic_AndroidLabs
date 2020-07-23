6f35953a11fa4aeae31716a254ade044c76360be
Daphne <ms.macer42@gmail.com>
2020-07-23 13:48:11 -0400

    The second commit of Lab6: Adding UV rate and correct Bitmap input on the UI

--- app/src/main/java/com/example/androidlabs/WeatherForecast.java
+++ app/src/main/java/com/example/androidlabs/WeatherForecast.java
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -37,7 +46,7 @@ public class WeatherForecast extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
-        new ForecastQuery().execute("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric", "http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389");
+        new ForecastQuery().execute();
 
     }
 
@@ -50,15 +59,23 @@ public class WeatherForecast extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-//***** https://stackoverflow.com/questions/9605913/how-do-i-parse-json-in-android
+
+    public boolean fileExistance(String fname){
+        File file = getBaseContext().getFileStreamPath(fname);
+        return file.exists();
+    }
+
+    //***** https://stackoverflow.com/questions/9605913/how-do-i-parse-json-in-android
 //                                        Type1, Type2   Type3
 private class ForecastQuery extends AsyncTask<String, Integer, String> {
     ProgressBar pb;
-    private String UV;
+    private String UVJSON, UV;
     private String minTemp;
     private String maxTemp;
     private String currentTemp;
     private Bitmap image;
+    private String iconName;
+    private URL url;
 
     protected void onPreExecute() {
         super.onPreExecute();
@@ -74,72 +91,136 @@ private class ForecastQuery extends AsyncTask<String, Integer, String> {
         TextView textViewItem = null;
 
         try {
-            for (int i = 0; i < args.length; i++){
-
-                URL url = new URL(args[i]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-                if(i == 0){
-                    InputStream inStream = connection.getInputStream();
-
-                    //Set up the XML parser:
-                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                    factory.setNamespaceAware(false);
-                    XmlPullParser xpp = factory.newPullParser();
-                    xpp.setInput( inStream  , "UTF-8");
-
-                    //Iterate over the XML tags:
-                    int EVENT_TYPE;         //While not the end of the document:
-                    while((EVENT_TYPE = xpp.getEventType()) != XmlPullParser.END_DOCUMENT)
-                    {
-                        switch(EVENT_TYPE)
-                        {
-                                case START_TAG:         //This is a start tag < ... >
-                                String tagName = xpp.getName(); // What kind of tag?
-
-                                //<temperature value="29.69" min="29" max="30" unit="celsius"/>
-                                if(tagName.equals("temperature"))
-                                {
-
-                                    pretendToWait(1);
-
-                                    currentTemp = xpp.getAttributeValue(null, "value"); //What is the String associated with message?
-                                    publishProgress(25);
-
-                                    pretendToWait(1);
-
-                                    minTemp = xpp.getAttributeValue(null, "min"); //What is the String associated with message?
-                                    publishProgress(50);
-
-                                    pretendToWait(1);
-
-                                    maxTemp = xpp.getAttributeValue(null, "max"); //What is the String associated with message?
-                                    publishProgress(75);
-
-                                    pretendToWait(1);
+            url = new URL("http://api.openweathermap.org/data/2.5/weather?q=ottawa,ca&APPID=7e943c97096a9784391a981c4d878b22&mode=xml&units=metric");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
 
 
+            InputStream inStream = connection.getInputStream();
 
-                                }
-                                break;
-                            case END_TAG:           //This is an end tag: </ ... >
-                                break;
-                            case TEXT:              //This is text between tags < ... > Hello world </ ... >
-                                break;
+            //Set up the XML parser:
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(false);
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput( inStream  , "UTF-8");
+
+            //Iterate over the XML tags:
+            int EVENT_TYPE;         //While not the end of the document:
+            while((EVENT_TYPE = xpp.getEventType()) != XmlPullParser.END_DOCUMENT) {
+                switch (EVENT_TYPE) {
+                    case START_TAG:         //This is a start tag < ... >
+                        String tagName = xpp.getName(); // What kind of tag?
+
+                        //<temperature value="29.69" min="29" max="30" unit="celsius"/>
+                        if (tagName.equals("temperature")) {
+
+                            pretendToWait(1);
+
+                            currentTemp = xpp.getAttributeValue(null, "value"); //What is the String associated with message?
+                            publishProgress(25);
+
+                            pretendToWait(1);
+
+                            minTemp = xpp.getAttributeValue(null, "min"); //What is the String associated with message?
+                            publishProgress(50);
+
+                            pretendToWait(1);
+
+                            maxTemp = xpp.getAttributeValue(null, "max"); //What is the String associated with message?
+                            publishProgress(75);
+
+                            pretendToWait(1);
+
+
+                        } else if (tagName.equals("weather")) {
+                            iconName = xpp.getAttributeValue(null, "icon"); //What is the String associated with message?
+
                         }
-                        xpp.next(); // move the pointer to next XML element
-                    }
-                } else if(i == 1){
-
+                        break;
+                    case END_TAG:           //This is an end tag: </ ... >
+                        break;
+                    case TEXT:              //This is text between tags < ... > Hello world </ ... >
+                        break;
                 }
-
+                xpp.next(); // move the pointer to next XML element
             }
-
         }
         catch(MalformedURLException mfe){ ret = "Malformed URL exception"; }
         catch(IOException ioe)          { ret = "IO Exception. Is the Wifi connected?";}
         catch(XmlPullParserException pe){ ret = "XML Pull exception. The XML is not properly formed" ;}
+
+        BufferedReader reader = null;
+
+        try {
+            url = new URL("http://api.openweathermap.org/data/2.5/uvi?appid=7e943c97096a9784391a981c4d878b22&lat=45.348945&lon=-75.759389");
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line+"\n");
+            }
+
+            UVJSON = buffer.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        try{
+            // get image
+            if(!fileExistance(iconName + ".png")){
+                url = new URL("http://openweathermap.org/img/w/" + iconName + ".png");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode == 200) {
+                    image = BitmapFactory.decodeStream(connection.getInputStream());
+                }
+
+                FileOutputStream outputStream = openFileOutput(iconName + ".png", Context.MODE_PRIVATE);
+                image.compress(Bitmap.CompressFormat.PNG, 80, outputStream);
+                outputStream.flush();
+                outputStream.close();
+            } else {
+
+                FileInputStream fis = null;
+                try {
+                    fis = openFileInput(iconName + ".png");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                image = BitmapFactory.decodeStream(fis);
+            }
+
+            publishProgress(100);
+
+        }
+        catch(MalformedURLException mfe){ ret = "Malformed URL exception"; }
+        catch(IOException ioe)          { ret = "IO Exception. Is the Wifi connected?";}
+
         //What is returned here will be passed as a parameter to onPostExecute:
         return ret;
     }
@@ -160,6 +241,20 @@ private class ForecastQuery extends AsyncTask<String, Integer, String> {
 
                 textViewItem = (TextView)findViewById(R.id.textViewMaxTemp);
                 textViewItem.setText("Max: " + Math.round(Double.parseDouble(maxTemp)));
+
+                ImageView imageView = findViewById(R.id.imageViewID);
+                imageView.setImageBitmap(image);
+
+                JSONObject jObject;
+
+                try {
+                    jObject = new JSONObject(UVJSON);
+                    UV = jObject.getString("value");
+                    textViewItem = (TextView)findViewById(R.id.textViewUVID);
+                    textViewItem.setText("UV rating: " + Math.round(Double.parseDouble(UV)));
+
+                } catch (JSONException e){
+                }
 
             }
         });
